98cdae21c5107a60e88eb49126f63d5f7b88738d
Amr Fawaz <amr.abdelhakim.fawaz+75@gmail.com>
2021-11-10 09:45:51 +0200

    change landing page

--- Eva/AppDelegate/AppDelegate.swift
+++ Eva/AppDelegate/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UIView.appearance().semanticContentAttribute = .forceRightToLeft
+        RunLoop.current.run(until: NSDate(timeIntervalSinceNow:3) as Date)
+
         StartUpCoordinator().start()
+
+
         return true
     }
 
